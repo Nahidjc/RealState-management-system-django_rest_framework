a49b8756c2f32f97cd019b08ae37312b98d626f2
Nahidjc <nahidjc80@gmail.com>
2021-09-19 02:46:31 +0600

    registration successfully

--- realstate_management_django_rest_framework/urls.py
+++ realstate_management_django_rest_framework/urls.py
@@ -8,10 +8,10 @@ from rest_framework_simplejwt.views import (
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/auth/', include('accounts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('accounts/', include('accounts.urls')),
+
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
