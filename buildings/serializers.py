cd4b5869b465eb242b84b55954925977ee6aaab8
Nahidjc <nahidjc80@gmail.com>
2021-09-19 18:42:22 +0600

    building app serializers

--- buildings/serializers.py
+++ buildings/serializers.py
@@ -1,9 +1,24 @@
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
-# from .models import Agent
+from .models import Home, ImageFiles
 
 
-class AgentSerializer(serializers.ModelSerializer):
+class HomeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Agent
+        model = Home
+        fields = ('title', 'slug', 'photo', 'bathrooms', 'bedrooms', 'sqft',
+                  'state', 'zipcode', 'address', 'city', 'sale_type', 'home_type', 'price')
+
+
+class HomeDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Home
         fields = '__all__'
+        lookup_field = 'slug'
+
+
+class ImageFilesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImageFiles
+        fields = '__all__'
+        lookup_field = 'home'
